167dbb84291e54252cb07a85f57988080c1459fa
Ginny Gao <qinqingao1a@hotmail.com>
2018-12-29 13:03:20 -0500

    Add files via upload

--- CreditDefault/AutoML.R
+++ CreditDefault/AutoML.R
@@ -3,39 +3,35 @@ library(h2o)
 h2o.init()
 
 
-library(class)
+# library(class)
 library(caret)
 library(gains)
 library(rlang)
 library(geiger)
-library(corrplot)
-library(adabag)
 library(data.table)
-library(ggplot2)    
-library(Ckmeans.1d.dp)
-library(xgboost)
+
 
 setwd('/Users/qinqingao/Desktop/Columbia/Courses/Fall 2018/EECS 6690/Project')
-require(gdata)
+# require(gdata)
 
-raw <- read.xls ("default of credit card clients.xls", sheet = 1, skip = 1, row.names = 1)
-dim(raw)
-# [1] 30000    24
+# raw <- read.xls ("default of credit card clients.xls", sheet = 1, skip = 1, row.names = 1)
+# dim(raw)
+# # [1] 30000    24
 
-#check any null value, none (30000 * 24 = 720000)
-table(is.na(raw))
-#  FALSE 
-# 720000 
+# #check any null value, none (30000 * 24 = 720000)
+# table(is.na(raw))
+# #  FALSE 
+# # 720000 
 
-#train-test split, 80%-20%
-set.seed(2018)
-sample_row_num <- sample(nrow(raw), nrow(raw) * 0.8)
+# #train-test split, 80%-20%
+# set.seed(2018)
+# sample_row_num <- sample(nrow(raw), nrow(raw) * 0.8)
 
-train <- raw[sample_row_num, ]
-test <- raw[-sample_row_num, ]  
+# train <- raw[sample_row_num, ]
+# test <- raw[-sample_row_num, ]  
  
-train_label <- train[, ncol(train)]
-test_label <- test[, ncol(test)]
+# train_label <- train[, ncol(train)]
+# test_label <- test[, ncol(test)]
 
 
 #export train, test to csv for h2o import
@@ -44,8 +40,8 @@ test_label <- test[, ncol(test)]
 
 
 # Import train/test set into H2O
-train <- h2o.importFile('train.csv')
-test <- h2o.importFile('test.csv')
+train <- h2o.importFile('/Users/qinqingao/Desktop/Columbia/Courses/Fall 2018/EECS 6690/Project/train.csv')
+test <- h2o.importFile('/Users/qinqingao/Desktop/Columbia/Courses/Fall 2018/EECS 6690/Project/test.csv')
 
 
 #normalizing numerical variables
@@ -65,13 +61,21 @@ for(name in names(train)){
 
 
 
-# Identify predictors and response
+# Identify response and predictors
 y <- "default.payment.next.month"
 x <- setdiff(names(train_norm), y)
 
 # For binary classification, response should be a factor
 train_norm[,y] <- as.factor(train_norm[,y])
 valid_norm[,y] <- as.factor(valid_norm[,y])
+
+
+#########################
+
+#    5.1  AML Train     # 
+
+#########################
+
 
 # Run AutoML for 20 base models (limited to 1 hour max runtime by default)
 aml <- h2o.automl(x = x, y = y,
@@ -236,10 +240,10 @@ aml@leader
 # Gains/Lift Table: Extract with `h2o.gainsLift(<model>, <data>)` or `h2o.gainsLift(<model>, valid=<T/F>, xval=<T/F>)`
 
 
-pred <- h2o.predict(aml, test)  # predict(aml, test) also works
+# pred <- h2o.predict(aml, train_norm)  # predict(aml, test) also works
 
 # or:
-pred <- h2o.predict(aml@leader, test)
+pred <- h2o.predict(aml@leader, train_norm)
 #   predict        p0        p1
 # 1       0 0.8061128 0.1938872
 # 2       1 0.7753841 0.2246159
@@ -249,6 +253,256 @@ pred <- h2o.predict(aml@leader, test)
 # 6       1 0.7753841 0.2246159
 
 # [6000 rows x 3 columns] 
+
+
+#find range of p1 given predict == 1
+range(as.data.frame(pred)[which(as.data.frame(pred)[,1] == 1), ][,3])
+# [1] 0.2283783 0.9107308
+
+#confusion matrix for train
+PredAMLLabel = ifelse(pred[,3] > 0.5, 1, 0)
+err <- 1 - confusionMatrix(table(as.vector(PredAMLLabel), as.vector(train_norm[,y])))$overall['Accuracy']
+err
+# 0.176375
+
+# Confusion Matrix and Statistics
+
+   
+#         0     1
+#   0 18054  2991
+#   1   627  2328
+                                          
+#                Accuracy : 0.8492          
+#                  95% CI : (0.8447, 0.8538)
+#     No Information Rate : 0.7784          
+#     P-Value [Acc > NIR] : < 2.2e-16       
+                                          
+#                   Kappa : 0.4805          
+#  Mcnemar's Test P-Value : < 2.2e-16       
+                                          
+#             Sensitivity : 0.9664          
+#             Specificity : 0.4377          
+#          Pos Pred Value : 0.8579          
+#          Neg Pred Value : 0.7878          
+#              Prevalence : 0.7784          
+#          Detection Rate : 0.7522          
+#    Detection Prevalence : 0.8769          
+#       Balanced Accuracy : 0.7021          
+                                          
+#        'Positive' Class : 0    
+
+
+
+PredAMLLabel = data.frame(as.numeric(as.vector(PredAMLLabel)))
+names(PredAMLLabel) <- "PredAMLLabel"
+
+PredAMLScore = as.vector(pred[,3])
+length(PredAMLScore)
+
+PredAMLScore = data.frame(as.vector(PredAMLScore))
+names(PredAMLScore) <- "PredAMLScore"
+
+
+#normalizing numerical variables
+scale01 <- function(x){
+     (x - min(x)) / (max(x) - min(x))
+ }
+
+train_norm = train
+valid_norm = test
+
+for(name in names(train)){
+     if(name != "default.payment.next.month"){
+         train_norm[name] <- scale01(train_norm[name])
+         valid_norm[name] <- scale01(valid_norm[name])
+     }     
+ }
+
+
+train_norm = data.frame(as.data.frame(train_norm), PredAMLLabel, PredAMLScore)
+head(train_norm)
+
+gtt = gains(actual=as.numeric(train_norm[,y]), predicted=train_norm$PredAMLScore,optimal=TRUE)
+cpt_y = gtt$cume.pct.of.total
+cpt_x = gtt$depth
+
+predicted = table(train_norm$PredAMLLabel)[2]
+predicted
+
+xx = cpt_x / 100 * 24000
+yy = cpt_y * predicted
+
+xx = prepend(xx,0,before=1)
+yy = prepend(yy,0,before=1)
+fit = lm(yy ~ poly(xx,3,raw=TRUE))
+
+xx = 0:24000
+model_yy = predict(fit,data.frame(xx))
+
+
+# png("/Users/qinqingao/Desktop/Columbia/Courses/Fall 2018/EECS 6690/Project/figs/AML_gain_chart_train.png")
+plot(xx, model_yy, col="green",xlab="Number of total data", ylab="Cumulative number of target data",
+	type = 'l', lwd = 3)
+
+best_yy = rep(predicted, 24001)
+for(i in 0:predicted){
+  best_yy[i+1] = i
+}
+
+lines(xx,best_yy,col="red",lwd=2)
+
+base_yy = predicted / 24000 * xx
+lines(xx,base_yy,col="blue", lwd = 2)
+
+legend('bottomright', legend=c("best curve","model", "baseline"), col = c("red","green","blue"), lwd=c(1,1,1),cex=1)
+title("Gain chart of AML (training)")
+# dev.off()
+
+
+#Calculate area ratio
+a1v = sum(model_yy-base_yy)
+a2v = sum(best_yy-base_yy)
+a1v/a2v
+# [1] 0.6851915
+
+
+#########################
+
+#    5.2  AML Test     # 
+
+#########################
+
+
+predv <- h2o.predict(aml@leader, valid_norm)
+
+#find range of p1 given predict == 1
+range(as.data.frame(pred)[which(as.data.frame(predv)[,1] == 1), ][,3])
+# [1] 0.2283783 0.9107308
+
+#confusion matrix for test
+PredAMLLabelV = ifelse(predv[,3] > 0.5, 1, 0)
+errv <- 1 - confusionMatrix(table(as.vector(PredAMLLabelV), as.vector(valid_norm[,y])))$overall['Accuracy']
+errv
+# 0.1795
+
+
+PredAMLLabelV = data.frame(as.numeric(as.vector(PredAMLLabelV)))
+names(PredAMLLabelV) <- "PredAMLLabelV"
+
+PredAMLScoreV = as.vector(predv[,3])
+length(PredAMLScoreV)
+
+PredAMLScoreV = data.frame(as.vector(PredAMLScoreV))
+names(PredAMLScoreV) <- "PredAMLScoreV"
+
+
+# #normalizing numerical variables
+# scale01 <- function(x){
+#      (x - min(x)) / (max(x) - min(x))
+#  }
+
+# train_norm = train
+# valid_norm = test
+
+# for(name in names(train)){
+#      if(name != "default.payment.next.month"){
+#          train_norm[name] <- scale01(train_norm[name])
+#          valid_norm[name] <- scale01(valid_norm[name])
+#      }     
+#  }
+
+
+valid_norm = data.frame(as.data.frame(valid_norm), PredAMLLabelV, PredAMLScoreV)
+head(valid_norm)
+
+gtt = gains(actual=as.numeric(valid_norm[,y]), predicted=valid_norm$PredAMLScoreV,optimal=TRUE)
+cpt_y = gtt$cume.pct.of.total
+cpt_x = gtt$depth
+
+predictedV = table(valid_norm$PredAMLLabelV)[2]
+predictedV
+
+xx = cpt_x / 100 * 6000
+yy = cpt_y * predictedV
+
+xx = prepend(xx,0,before=1)
+yy = prepend(yy,0,before=1)
+fit = lm(yy ~ poly(xx,3,raw=TRUE))
+
+xx = 0:6000
+model_yy = predict(fit,data.frame(xx))
+
+
+# png("/Users/qinqingao/Desktop/Columbia/Courses/Fall 2018/EECS 6690/Project/figs/AML_gain_chart_test.png")
+plot(xx, model_yy, col="green",xlab="Number of total data", ylab="Cumulative number of target data",
+	type = 'l', lwd = 3)
+
+best_yy = rep(predictedV, 6001)
+for(i in 0:predictedV){
+  best_yy[i+1] = i
+}
+
+lines(xx,best_yy,col="red",lwd=2)
+
+base_yy = predictedV / 6000 * xx
+lines(xx,base_yy,col="blue", lwd = 2)
+
+legend('bottomright', legend=c("best curve","model", "baseline"), col = c("red","green","blue"), lwd=c(1,1,1),cex=1)
+title("Gain chart of AML (validation)")
+# dev.off()
+
+
+#Calculate area ratio
+a1v = sum(model_yy-base_yy)
+a2v = sum(best_yy-base_yy)
+a1v/a2v
+# [1] 0.4645352
+
+
+#Part 5.2.3: Use SSM(Sorting Smoothing Method) to estimate real probability
+#1. order the valid data according to predictive probability
+valid_norm_sort = valid_norm[order(PredAMLScoreV), ]
+#2. use SSM formula to evaluate actural probability 'Pi', we choose n =50 according to the paper
+VALIDSIZE = dim(valid_norm)[1]
+
+n = 50
+actural_p_valid = rep(0, VALIDSIZE)
+pred_valid = round(valid_norm_sort$PredAMLScoreV)
+pred_valid = prepend(pred_valid, rep(0, n), before=1)
+pred_valid = append(pred_valid, rep(0, n))
+
+for(i in 1:VALIDSIZE){
+  actural_p_valid[i] = sum(pred_valid[i:(i + 2 * n)])/(2 * n + 1)
+}
+valid_norm_sort = data.frame(valid_norm_sort, actural_p_valid)
+
+
+# png("/Users/qinqingao/Desktop/Columbia/Courses/Fall 2018/EECS 6690/Project/figs/Scatter plot diagram of AML.png")
+plot(valid_norm_sort$PredAMLScoreV, valid_norm_sort$actural_p_valid,
+	xlab = "Predicted Probability", ylab = "Actual probability")
+
+yy = valid_norm_sort$actural_p_valid
+xx = valid_norm_sort$PredAMLScoreV
+actual_fit = lm(yy ~ xx)
+
+xx = seq(0, 1 : 0.1)
+yy = predict(actual_fit, data.frame((xx)))
+lines(xx, yy)
+
+summary(actual_fit)
+legend('topleft',legend=c("y = 1.542782x - 0.252073","R^2 = 0.827"))
+title('Actual vs Pred Probability of AML (validation)')
+# dev.off()
+
+
+
+
+
+
+
+
+
+
 
 
 
